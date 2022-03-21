1d70c6ad538722be74b7156e8cb09bcd528bccc4
anypad <86584934+anypad@users.noreply.github.com>
2022-03-21 21:53:18 +0530

    Add files via upload

---
+++ upcomingProjects.ts
@@ -0,0 +1,75 @@
+export const upcomingProjects = [
+  // {
+  //   title: "Amara",
+  //   description:
+  //     "Amara Finance is a cross-chain financial aggregator for NextDeFi aiming to be the DeFi collaboration center to bridge Polkadot and the off-DOT world.",
+  //   targetRaise: "100k",
+  //   tokenPrice: "TBA",
+  //   idoDate: "04 March 2022",
+  //   twitter: "https://twitter.com/AmaraFinance",
+  //   website: "https://www.amara.link/",
+  //   telegram: "http://t.me/AmaraFinance",
+  //   whitelist: "https://forms.gle/N9SkrNsJHKSaMBfA7",
+  // },
+  {
+    title: "Fota",
+    description:
+      "FOTA is a AAA Metaverse Gaming Esport project under the Hybrid of MOBA-RPG that is integrated with MicrosoftMesh Tech",
+    targetRaise: "75k",
+    tokenPrice: "TBA",
+    idoDate: "TBA",
+    twitter: "https://twitter.com/fightoftheages",
+    website: "https://fota.io/",
+    telegram: "https://t.me/fota_groupchat",
+    whitelist: "https://forms.gle/bimERUwh1ts5z4XT9",
+  },
+  {
+    title: "Animalia",
+    description:
+      "Animalia is an independent free-to-play online Nft trading card game featuring crypto-inspired meme creatures and gemstones. Powered by the BNB Chain",
+    targetRaise: "50k",
+    tokenPrice: "TBA",
+    idoDate: "TBA",
+    twitter: "https://twitter.com/Animalia_token",
+    website: "https://animalia.games/",
+    telegram: "https://t.me/Animalia_ANIM",
+    whitelist: "",
+  },
+  // {
+  //   title: "Stman",
+  //   description:
+  //     "Revolutionary of P2E Game. Say 'No' with inflation, just enjoy a free-to-play-and-earn mechanism with StickMan game crypto",
+  //   targetRaise: "50k",
+  //   tokenPrice: "TBA",
+  //   idoDate: "TBA",
+  //   twitter: "https://twitter.com/Stickman_STMAN",
+  //   website: "https://stman.io/",
+  //   telegram: "https://t.me/stman_official",
+  //   whitelist: "",
+  // },
+  {
+    title: "Crypcade",
+    description:
+      "CrypCade “is the Uber and Airbnb of Arcade”, being an economically sustainable Gaming/ Entertainment ecosystem.",
+    targetRaise: "100k",
+    tokenPrice: "TBA",
+    idoDate: "01 March 2022",
+    twitter: "https://twitter.com/CrypCade",
+    website: "https://crypcade.io/",
+    telegram: "https://t.me/CrypCade",
+    whitelist: "",
+  },
+  {
+    title: "Supernova",
+    description:
+      "Supernova is a sci-fi sandbox survival MMORPG in an open world featuring indirect controls and an open economy with play to earn mechanics!",
+    targetRaise: "100k",
+    tokenPrice: "TBA",
+    idoDate: "22 March 2022",
+    twitter: "https://twitter.com/SupernovaVerse",
+    website: "https://sunday.games/supernova/",
+    telegram: "https://t.me/+jNNGMc-lkzJlMjZi",
+    whitelist: "https://forms.gle/dqz1xB6Fz4T7jyaF8",
+  },
+
+];
