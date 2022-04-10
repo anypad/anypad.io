e3471f9293ffedae630ef32b0df95038207bb9bf
anypad <86584934+anypad@users.noreply.github.com>
2022-04-10 13:59:07 +0530

    Update upcomingProjects.ts

--- upcomingProjects.ts
+++ upcomingProjects.ts
@@ -1,18 +1,5 @@
 export const upcomingProjects = [
-  // {
-  //   title: "Amara",
-  //   description:
-  //     "Amara Finance is a cross-chain financial aggregator for NextDeFi aiming to be the DeFi collaboration center to bridge Polkadot and the off-DOT world.",
-  //   targetRaise: "100k",
-  //   tokenPrice: "TBA",
-  //   idoDate: "04 March 2022",
-  //   twitter: "https://twitter.com/AmaraFinance",
-  //   website: "https://www.amara.link/",
-  //   telegram: "http://t.me/AmaraFinance",
-  //   whitelist: "https://forms.gle/N9SkrNsJHKSaMBfA7",
-  // },
-  
-  {
+ {
     title: "Animalia",
     description:
       "Animalia is an independent free-to-play online Nft trading card game featuring crypto-inspired meme creatures and gemstones. Powered by the BNB Chain",
@@ -23,18 +10,5 @@ export const upcomingProjects = [
     website: "https://animalia.games/",
     telegram: "https://t.me/Animalia_ANIM",
     whitelist: "",
-  },
-  // {
-  //   title: "Stman",
-  //   description:
-  //     "Revolutionary of P2E Game. Say 'No' with inflation, just enjoy a free-to-play-and-earn mechanism with StickMan game crypto",
-  //   targetRaise: "50k",
-  //   tokenPrice: "TBA",
-  //   idoDate: "TBA",
-  //   twitter: "https://twitter.com/Stickman_STMAN",
-  //   website: "https://stman.io/",
-  //   telegram: "https://t.me/stman_official",
-  //   whitelist: "",
-  // },
-
+  }
 ];
