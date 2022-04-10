c1e05dc8c14a03debae900104b62e76177d3978e
anypad <86584934+anypad@users.noreply.github.com>
2022-04-10 21:03:24 +0530

    Add files via upload

---
+++ upcomingProjects.ts
@@ -0,0 +1,14 @@
+export const upcomingProjects = [
+  {
+    title: "Animalia",
+    description:
+      "Animalia is an independent free-to-play online Nft trading card game featuring crypto-inspired meme creatures and gemstones. Powered by the BNB Chain",
+    targetRaise: "50k",
+    tokenPrice: "TBA",
+    idoDate: "TBA",
+    twitter: "https://twitter.com/Animalia_token",
+    website: "https://animalia.games/",
+    telegram: "https://t.me/Animalia_ANIM",
+    whitelist: "",
+  },
+];
