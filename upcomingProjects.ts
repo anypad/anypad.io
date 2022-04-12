0fc44283828dbc64b95ffc597892969aa4e8c237
anypad <86584934+anypad@users.noreply.github.com>
2022-04-12 15:05:54 +0530

    Add files via upload

---
+++ upcomingProjects.ts
@@ -0,0 +1,26 @@
+export const upcomingProjects = [
+  {
+    title: "Animalia",
+    description:
+      "Animalia is an independent free-to-play online Nft trading card game featuring crypto-inspired meme creatures and gemstones. Powered by the BNB Chain",
+    targetRaise: "50k",
+    tokenPrice: "TBA",
+    idoDate: "TBA",
+    twitter: "https://twitter.com/Animalia_token",
+    website: "https://animalia.games/",
+    telegram: "https://t.me/Animalia_ANIM",
+    whitelist: "",
+  },
+  {
+    title: "Nomadland",
+    description:
+      "Nomadland is a blockchain-based NFTs Action Role-Playing & strategy game built on Unreal Engine. Users can explore the world of Nomadland where you can buy land, build farms, battle epic monsters, and much more",
+    targetRaise: "100k",
+    tokenPrice: "$0.25",
+    idoDate: "25th April 2022",
+    twitter: "https://twitter.com/nomadgame1",
+    website: "https://nomadland.to/",
+    telegram: "https://t.me/nomadlandofficial",
+    whitelist: "http://bit.ly/3JHSUu5",
+  },
+];
